349fb6d5f9fe6c1cf803664335139534bb4d78c5
Vadym Kurinnyi <vadym.kurinnyi@brillpower.com>
2023-12-13 02:19:35 +0000

    HealthChecks UI

--- Viter.Consumer/Program.cs
+++ Viter.Consumer/Program.cs
@@ -1,7 +1,9 @@
 using Azure.Messaging.EventHubs.Primitives;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Azure.Devices;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
-using Microsoft.Extensions.Options;
 using StackExchange.Redis;
 using Viter.Consumer.Consumer;
 using Viter.Consumer.Consumer.Data;
@@ -11,14 +13,29 @@ var builder = WebApplication.CreateBuilder(args);
 string connectionString = builder.Configuration["AzureAppConfig_ConnectionString"]!;
 
 // Load configuration from Azure App Configuration
-    builder.Configuration.AddAzureAppConfiguration(opt =>
+builder.Configuration.AddAzureAppConfiguration(opt =>
 {
     opt.Connect(connectionString)
         .Select(KeyFilter.Any)
         .Select(KeyFilter.Any, builder.Environment.EnvironmentName)
         .UseFeatureFlags();
 });
+
 var services = builder.Services;
+services.AddHealthChecks()
+.AddRedis(builder.Configuration.GetConnectionString("Redis")!)
+.AddAzureIoTHub(opt =>
+{
+    opt.AddConnectionString(builder.Configuration.GetConnectionString("DeviceRegistryManager")!)
+    .AddRegistryReadCheck();
+});
+services
+.AddHealthChecksUI(options =>
+{
+    options.SetEvaluationTimeInSeconds(30);
+    options.AddHealthCheckEndpoint("Healthcheck API", "/healthcheck");
+}).AddInMemoryStorage();
+
 services.AddSingleton<ConnectionMultiplexer>(sp =>
     ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis")!));
 services.AddSingleton<IDatabase>(sp => sp.GetRequiredService<ConnectionMultiplexer>().GetDatabase());
@@ -27,12 +44,16 @@ services.AddSingleton<CheckpointStore, RedisCheckpointStore>();
 services.AddSingleton<IEventBatchConsumer, TelemetryEventBatchConsumer>();
 services.AddSingleton<ITimeSeriesManager, TimeSeriesManager>();
 services.AddHostedService<SimpleBatchProcessor>();
-//api
-services.AddSingleton(
-    RegistryManager.CreateFromConnectionString(builder.Configuration["ConnectionString:DeviceRegistryManager"]));
+services.AddSingleton(RegistryManager.CreateFromConnectionString(builder.Configuration.GetConnectionString("DeviceRegistryManager")));
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello Consumer!");
+app.MapHealthChecks("/healthcheck", new()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.MapHealthChecksUI(options => options.UIPath = "/dashboard");
 
 await app.RunAsync();
